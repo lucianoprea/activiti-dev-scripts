4f8fe14f4b77815d52b6fc70946ab982d5fae6e9
Ryan Dawson <ryan.dawson@alfresco.com>
2017-11-01 09:00:38 +0000

    permissions and typo in infra docker-compose

--- start-docker-compose.sh
+++ start-docker-compose.sh
@@ -1,6 +1,6 @@
 cd activiti-cloud-examples
 cd docker
-docker-compose -f infrastructure.yml up -d
+docker-compose -f infrastructure-docker.yml up -d
 docker-compose -f rb-docker-compose.yml up -d
 cd ..
 cd ..
---
+++ stop-docker-compose.sh
@@ -0,0 +1,6 @@
+cd activiti-cloud-examples
+cd docker
+docker-compose -f infrastructure-docker.yml down -d
+docker-compose -f rb-docker-compose.yml down -d
+cd ..
+cd ..
